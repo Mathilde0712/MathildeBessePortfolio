2f97d1ae9c3dce124fe0aadea19f013132247652
Mathilde Besse <mathildebesse07@gmail.com>
2024-07-16 15:20:40 +0200

    rectification d'une erreur de frappe

--- src/app/_components/Spacing.tsx
+++ src/app/_components/Spacing.tsx
@@ -1,16 +1,17 @@
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
-export type SpacingProps ={
-    size? : "sm" | "md" | "lg"
-}
+export type SpacingProps = {
+  size?: "sm" | "md" | "lg";
+};
 
-export const Spacing =({size ="md"} : SpacingProps) =>{
-    return <div className={cn({
-        "h-8 lg:h-16" : size === "sm",
-        "h-16 lg:h-24" : size=== "md",
-        "h-24 lg:h32" : size=== "lg"
-
-    })} />
-    
-    
-}
+export const Spacing = ({ size = "md" }: SpacingProps) => {
+  return (
+    <div
+      className={cn({
+        "h-8 lg:h-16": size === "sm",
+        "h-16 lg:h-24": size === "md",
+        "h-24 lg:h-32": size === "lg",
+      })}
+    />
+  );
+};
